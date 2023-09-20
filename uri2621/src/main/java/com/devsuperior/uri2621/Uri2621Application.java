fa73e3c65258f2dad630f5af4f9510cb239d91ca
truelanz <devalanoliveira@gmail.com>
2023-09-19 21:04:28 -0300

    uri2621

--- uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2621;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 @SpringBootApplication
@@ -20,5 +25,21 @@ public class Uri2621Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//SQL
+		System.out.println("\nSQL Query");
+		List<ProductMinProjection> list = repository.search1(10, 20, "P"); 
+		List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+
+		for(ProductMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		//JPQL
+		System.out.println("\nJPQL Query");
+		List<ProductMinDTO> result2 = repository.search2(10, 20, "P"); 
+
+		for(ProductMinDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }
